2506747317a3ec81b5d8096122fce73ba79037e1
Konstantin Voykov <koykov0@gmail.com>
2020-03-30 01:31:44 +0300

    Add struct walker generator.

--- compiler_test.go
+++ compiler_test.go
@@ -1,9 +1,13 @@
 package inspector
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestCompiler(t *testing.T) {
-	c := NewCompiler("github.com/koykov/inspector/testobj", "")
+	buf := &bytes.Buffer{}
+	c := NewCompiler("github.com/koykov/inspector/testobj", "", buf)
 	err := c.Compile()
 	if err != nil {
 		t.Error(err)
