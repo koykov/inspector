e84dd693e8ffcf9c13213e37d611f05e40ea51cd
Konstantin Voykov <koykov0@gmail.com>
2020-05-03 02:20:07 +0300

    Sync constants with tpl package.

--- cmp.go
+++ cmp.go
@@ -5,13 +5,13 @@ package inspector
 type Op int
 
 const (
-	OpUnk Op = iota
-	OpEq
-	OpNq
-	OpGt
-	OpGtq
-	OpLt
-	OpLtq
-	OpInc
-	OpDec
+	OpUnk Op = 0
+	OpEq  Op = 1
+	OpNq  Op = 2
+	OpGt  Op = 3
+	OpGtq Op = 4
+	OpLt  Op = 5
+	OpLtq Op = 6
+	OpInc Op = 7
+	OpDec Op = 8
 )
