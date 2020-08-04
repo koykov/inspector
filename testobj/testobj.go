52e01317099f3c3b12d8c26df9cd28c7639ae5e8
Konstantin Voykov <koykov0@gmail.com>
2020-08-05 00:22:34 +0300

    Add clear methods of testing objects.

--- testobj/testobj.go
+++ testobj/testobj.go
@@ -27,3 +27,34 @@ type TestObject struct {
 	Flags       TestFlag
 	Finance     *TestFinance
 }
+
+func (h *TestHistory) Clear() {
+	h.DateUnix, h.Cost = 0, 0
+	h.Comment = h.Comment[:0]
+}
+
+func (f *TestFinance) Clear() {
+	f.MoneyIn, f.MoneyOut, f.Balance, f.AllowBuy = 0, 0, 0, false
+	for i := range f.History {
+		f.History[i].Clear()
+	}
+}
+
+func (o *TestObject) Clear() {
+	o.Id = ""
+	o.Name = o.Name[:0]
+	o.Status, o.Cost = 0, 0
+	if o.Permission != nil {
+		for k := range *o.Permission {
+			(*o.Permission)[k] = false
+		}
+	}
+	for k, v := range o.HistoryTree {
+		v.Clear()
+		o.HistoryTree[k] = v
+	}
+	for k := range o.Flags {
+		o.Flags[k] = 0
+	}
+	o.Finance.Clear()
+}
