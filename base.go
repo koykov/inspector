b172bb706684280f64b06b20cde85e2d94b42543
Konstantin Voykov <koykov0@gmail.com>
2022-09-06 18:11:42 +0300

    deepcopy: improve bufferize functions.

--- base.go
+++ base.go
@@ -1,18 +1,4 @@
 package inspector
 
-import "github.com/koykov/fastconv"
-
 // BaseInspector describes base struct.
 type BaseInspector struct{}
-
-func (b BaseInspector) Bufferize(buf, p []byte) ([]byte, []byte) {
-	off := len(buf)
-	buf = append(buf, p...)
-	return buf, buf[off:]
-}
-
-func (b BaseInspector) BufferizeString(buf []byte, s string) ([]byte, string) {
-	off := len(buf)
-	buf = append(buf, s...)
-	return buf, fastconv.B2S(buf[off:])
-}
