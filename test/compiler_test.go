52ed19d9b66d735f3a3271119c7b452280edbd2a
Konstantin Voykov <koykov0@gmail.com>
2023-07-23 23:35:55 +0300

    Make tests green.

--- test/compiler_test.go
+++ test/compiler_test.go
@@ -10,7 +10,7 @@ import (
 func TestCompiler(t *testing.T) {
 	t.Run("package", func(t *testing.T) {
 		buf := &bytes.Buffer{}
-		c := inspector.NewCompiler(inspector.TargetPackage, "github.com/koykov/inspector/testobj", "github.com/koykov/inspector/testobj_ins", "", nil, buf, nil)
+		c := inspector.NewCompiler(inspector.TargetPackage, "github.com/koykov/inspector/testobj", "github.com/koykov/inspector/testobj_ins", "", nil, true, buf, nil)
 		err := c.Compile()
 		if err != nil {
 			t.Error(err)
