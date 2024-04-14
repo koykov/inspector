8b5b966a0c8780d2d11b7157900f41dd21472cbf
Konstantin Voykov <koykov0@gmail.com>
2024-04-14 15:43:20 +0300

    Naming issues; const formatting.

--- const.go
+++ const.go
@@ -3,27 +3,28 @@ package inspector
 // Op describes operation type.
 type Op int
 
+const (
+	OpUnk Op = iota
+	OpEq
+	OpNq
+	OpGt
+	OpGtq
+	OpLt
+	OpLtq
+	OpInc
+	OpDec
+)
+
 // LoopCtl describes loop control type.
 type LoopCtl int
 
 const (
-	OpUnk Op = 0
-	OpEq  Op = 1
-	OpNq  Op = 2
-	OpGt  Op = 3
-	OpGtq Op = 4
-	OpLt  Op = 5
-	OpLtq Op = 6
-	OpInc Op = 7
-	OpDec Op = 8
-
-	LoopCtlNone = 0
-	LoopCtlBrk  = 1
-	LoopCtlCnt  = 2
-
-	Nil = "nil"
-
-	FloatPrecision = 1e-3
+	LoopCtlNone LoopCtl = iota
+	LoopCtlBrk
+	LoopCtlCnt
 )
+
+const Nil = "nil"
+const FloatPrecision = 1e-3
 
 var _, _, _, _ = OpUnk, OpInc, OpDec, LoopCtlNone
