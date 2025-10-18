3ba3fcbbfcf158067da9389689fec3220a1c68bc
Konstantin Voykov <koykov0@gmail.com>
2025-10-18 23:45:11 +0300

    append: improve basic implementation

--- compiler.go
+++ compiler.go
@@ -537,7 +537,7 @@ if p, ok := src.(**` + pname + `); ok { x = *p } else if p, ok := src.(*` + pnam
 	c.wl("_, _, _ = src, value, path")
 	if node.hasc {
 		c.wdl(funcHeaderAppend)
-		// ...
+		err = c.writeNodeAppend(node, "x", 0)
 	}
 	c.wl("return src, nil")
 	c.wdl("}")
@@ -1399,6 +1399,112 @@ func (c *Compiler) writeNodeLC(node_ *node, v, fn string, depth int) error {
 			c.wl("*result=", fn, "(", c.fmtVnb(node_, v, depth), ")")
 			c.wl("return nil")
 		}
+	}
+
+	return nil
+}
+
+func (c *Compiler) writeNodeAppend(node_ *node, v string, depth int) error {
+	if !node_.hasa {
+		return nil
+	}
+	depths := strconv.Itoa(depth)
+
+	if node_.ptr && node_.typ != typeSlice {
+		// Value may be nil on pointer types.
+		c.wl("if ", v, " == nil { return nil }")
+	}
+
+	if node_.typ == typeSlice {
+		c.wl("if len(path)==", depths, "{")
+		c.wl(c.fmtVnb(node_, v, depth), "=append(", c.fmtVnb(node_, v, depth), ",value)")
+		c.wl("return ", v, ", nil")
+		c.wl("}")
+	}
+
+	switch node_.typ {
+	case typeStruct:
+		for _, ch := range node_.chld {
+			if !ch.hasa {
+				continue
+			}
+			c.wl("if path[", depths, "] == ", `"`, ch.name, `" {`)
+			nv := v + "." + ch.name
+			chPtr := ch.ptr && (ch.typ == typeStruct || ch.typ == typeMap || ch.typ == typeSlice)
+			if chPtr {
+				c.wl("if ", nv, "!=nil{")
+			}
+			_ = c.writeNodeAppend(ch, nv, depth+1)
+			if chPtr {
+				c.wl("}")
+			}
+			c.wl("}")
+		}
+	case typeMap:
+		origPtr := node_.ptr
+		if depth == 0 {
+			node_.ptr = true
+		}
+		if !node_.mapv.hasa {
+			return nil
+		}
+		nv := "x" + strconv.Itoa(depth)
+		c.wl("if len(path) < ", strconv.Itoa(depth+1), " { return nil }")
+		if node_.mapk.typn == "string" {
+			// Key is string, simple case.
+			key := c.fmtP(node_.mapk, "path["+depths+"]", depth+1)
+			c.wl("if ", nv, ", ok := ", c.fmtV(node_, v), "[", key, "]; ok {")
+			c.wl("_ = ", nv)
+			err := c.writeNodeAppend(node_.mapv, nv, depth+1)
+			if err != nil {
+				return err
+			}
+			c.wl("}")
+		} else {
+			// Convert path value to the key type and try to find it in the map.
+			c.wl("var k ", node_.mapk.typn)
+			snippet, imports, err := StrConvSnippet("path["+depths+"]", node_.mapk.typn, node_.mapk.typu, "k")
+			c.regImport(imports)
+			if err != nil {
+				return err
+			}
+			c.wl(snippet)
+			c.wl(nv, " := ", c.fmtV(node_, v), "[", c.fmtP(node_.mapk, "k", depth+1), "]")
+			c.wl("_ = ", nv)
+			err = c.writeNodeAppend(node_.mapv, nv, depth+1)
+			if err != nil {
+				return err
+			}
+		}
+		node_.ptr = origPtr
+	case typeSlice:
+		if !node_.slct.hasa {
+			return nil
+		}
+		c.wl("if len(path) < ", strconv.Itoa(depth+1), " { return nil }")
+
+		nv := "x" + strconv.Itoa(depth)
+		c.wl("var i int")
+		snippet, imports, err := StrConvSnippet("path["+depths+"]", "int", "", "i")
+		c.regImport(imports)
+		if err != nil {
+			return err
+		}
+		c.wl(snippet)
+		c.wl("if len(", c.fmtVnb(node_, v, depth), ") > i {")
+		if node_.slct.ptr || c.isBuiltin(node_.slct.typn) {
+			c.wl(nv, " := ", c.fmtVd(node_, v, depth), "[i]")
+		} else {
+			c.wl(nv, " := &", c.fmtVd(node_, v, depth), "[i]")
+		}
+		c.wl("_ = ", nv)
+		err = c.writeNodeAppend(node_.slct, nv, depth+1)
+		if err != nil {
+			return err
+		}
+		c.wl("}")
+	case typeBasic:
+		// do nothing
 	}
 
 	return nil
